ba27ab2b7cb76c46695ce4d6c0052edd8cf28d95
vrbadev <vrba.vojtech@gmail.com>
2026-03-11 20:21:17 +0100

    Fixed service crashes

--- .ci/pkg_service/etc/ctu-mrs/mrs-uav-bluetooth/service_exec.sh
+++ .ci/pkg_service/etc/ctu-mrs/mrs-uav-bluetooth/service_exec.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -e
+
+export HOME=${HOME:-/root}
+export USER=${USER:-root}
+export LOGNAME=${LOGNAME:-root}
+export ROS_LOG_DIR=${ROS_LOG_DIR:-/var/log/mrs-uav-bluetooth}
+
+mkdir -p "$ROS_LOG_DIR"
+
 source /opt/ros/jazzy/setup.bash
-ros2 launch mrs_uav_bluetooth bluetooth_node.launch.py
+exec ros2 launch mrs_uav_bluetooth bluetooth_node.launch.py
